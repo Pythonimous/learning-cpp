436beb2751319d297187548b5e8699d96a2af4cf
Kirill Nikolaev <kir.nikolaev.7@gmail.com>
2021-02-23 12:55:28 +0300

    pointers arrays 3

---
+++ crash_course/6.pointarr_1.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int* bubbleSort(int *pArr, int n) {
+  --n;
+  for (n; n > 0; --n) {
+    for (int i = 0; i < n; ++i) {
+      if (*(pArr+n) < *(pArr+i)) {
+        int temp = *(pArr+i);
+        *(pArr+i) = *(pArr+n);
+        *(pArr+n) = temp;
+      }
+    }
+  }
+  return pArr;
+}
+
+void printMatrix(int n)
+{
+  int matrix[5][5];
+
+  for (int i = 0; i < 25; ++i) {
+    matrix[i/5][i%5] = i+1;
+  }
+  
+  for (int i = 0; i < 5; ++i) {
+    for (int j = 0; j < 5; ++j) {
+      cout << matrix[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void diagMatrix(int n)
+{
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      short o;
+      if (i == j) { o = 0; }
+      if (i < j)  { o = 1; }
+      if (i > j)  { o = -1; }
+      cout << o << " ";
+      }
+    cout << "\n";
+  }
+}
+
+void printPascalTr(int size) {
+  int *pRow = new int[size];
+  pRow[0] = 1;
+  for (int i = 0; i < size; ++i) {
+    for (int j = i; j >= 0; --j) {
+      if (j-1 >= 0) { pRow[j] += pRow[j-1]; }
+    }
+    for (int j = 0; j <= i; ++j) {
+      cout << pRow[j] << " ";
+    }
+    cout << "\n";
+  }
+  delete[] pRow;
+  pRow = NULL;
+}
+
+void printBoard(int**pBoard, int row, int col){
+  for (int i = 0; i < row; ++i) {
+    for (int j = 0; j < col; ++j) {
+      cout << pBoard[i][j] << " ";
+    }
+    cout << "\n";
+  }
+}
+
+void makeBoard(int row, int col) {
+  int **pBoard = new int*[row]; // declaring board and dynamically allocating row space in heap
+  for (int i = 0; i < row; ++i) {
+    pBoard[i] = new int[col]; // dynamically allocating columns
+    for (int j = 0; j < col; ++j) {
+      pBoard[i][j] = 1;
+    }
+  }
+  printBoard(pBoard, row, col);
+  delete[] pBoard;
+  pBoard = NULL;
+}
+
+int main() {
+  cout << "Matrix of numbers: \n";
+  printMatrix(0);
+
+  int d;
+  cout << "Diagonal magrix of size: ";
+  cin >> d;
+  cout << endl;
+  diagMatrix(d);
+
+  int s;
+  cout << "Enter the size of Pascal triangle: ";
+  cin >> s;
+  printPascalTr(s);
+  return 0;
+}
---
+++ crash_course/6.pointarr_2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+void withdraw(double* balance, double amount) {
+  if (*balance < amount) {
+    cout << "You are broke. You don't have enough money. Go away" << endl;
+  }
+  else { *balance -= amount; }
+}
+
+void deposit(double* balance, double amount) { *balance += amount; }
+
+
+int main() {
+
+  double *balance;
+
+  double harry_account_balance = 0;
+
+  balance = &harry_account_balance;
+
+  cout << "Harry's Balance at start is: " << harry_account_balance;
+  cout <<endl;
+  double amount  = 1000.00; //setting the amount to be deposited
+
+  deposit(balance, amount);
+
+  cout << "Balance in harry account is: " << harry_account_balance;
+  cout<<endl;
+  amount = 500; //setting amount to 500
+  withdraw(balance, amount);
+
+  cout << "Balance after withdrawing cash is: " << harry_account_balance;
+  cout<<endl;
+  amount = 3000; //setting amount to 3000
+  
+  cout << "Withdrawing "<<amount << " now" <<endl;
+
+  withdraw(balance, amount);
+  
+  return 0;
+}
